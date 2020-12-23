54d83621bf82a21f985a2736e01fac064da8a2ce
yoshi28012007 <yoshi28012007@gmail.com>
2020-12-23 12:59:37 -0500

    Routing
    Routing en Angular

--- clientes-app/src/app/app.module.ts
+++ clientes-app/src/app/app.module.ts
@@ -5,15 +5,31 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ClientesComponent } from './clientes/clientes.component';
 import { ClienteService } from './clientes/cliente.service';
+import { HeaderComponent } from './header/header.component';
+
+//GEP-INI
+import { RouterModule, Routes } from '@angular/router';
+import { DirectivaComponent } from './directiva/directiva.component';
+
+const routes : Routes = [
+  {path:'', redirectTo: '/clientes', pathMatch: 'full'},
+  {path: 'clientes' , component: ClientesComponent},
+  {path: 'directivas', component: DirectivaComponent}
+];
+
+//GEP-FIN
 
 @NgModule({
   declarations: [
     AppComponent,
-    ClientesComponent
+    ClientesComponent,
+    HeaderComponent,
+    DirectivaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [ ClienteService],
   bootstrap: [AppComponent]
